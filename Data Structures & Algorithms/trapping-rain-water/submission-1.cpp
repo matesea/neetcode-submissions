81d4a3954b0ddeb6a17d8c9e3e783257e22f28d8
Wen-Long(Neil) Yang <matesea@gmail.com>
2026-04-29 12:00:36 +0800

    Add: trapping-rain-water - submission-1

---
+++ Data Structures & Algorithms/trapping-rain-water/submission-1.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    int trap(vector<int>& height) {
+        int n = height.size();
+        vector<int> left(n, 0), right(n, 0);
+        for (int i = 1; i < n; ++i)
+            left[i] = max(left[i-1], height[i-1]);
+        for (int i = n-2; i >= 0; --i)
+            right[i] = max(right[i+1], height[i+1]);
+        int res = 0;
+        for (int i = 0; i < n; ++i) {
+            res += max(min(left[i], right[i]) - height[i], 0);
+        }
+        return res;
+    }
+};
