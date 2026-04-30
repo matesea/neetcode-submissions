c4c72a512b791122436f9b2397f478e09b5d11fb
Wen-Long(Neil) Yang <matesea@gmail.com>
2026-04-30 12:34:37 +0800

    Add: daily-temperatures - submission-0

---
+++ Data Structures & Algorithms/daily-temperatures/submission-0.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    vector<int> dailyTemperatures(vector<int>& temperatures) {
+       stack<int> s; 
+       int n = temperatures.size();
+       vector<int> res(n, 0);
+       for (int i = 0; i < n; ++i) {
+        while (s.size() > 0 && temperatures[s.top()] < temperatures[i]) {
+            res[s.top()] = i - s.top();
+            s.pop();
+        }
+        s.push(i);
+       }
+       return res;
+    }
+};
