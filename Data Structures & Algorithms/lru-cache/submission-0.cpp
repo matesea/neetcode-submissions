23c1192768bd50d46020fde0b56683f1c2c831a3
Wen-Long(Neil) Yang <matesea@gmail.com>
2026-05-02 16:44:03 +0800

    Add: lru-cache - submission-0

---
+++ Data Structures & Algorithms/lru-cache/submission-0.cpp
@@ -0,0 +1,30 @@
+class LRUCache {
+    unordered_map<int,pair<int,list<int>::iterator>> cache;
+    list<int> order;
+    int cap;
+public:
+    LRUCache(int capacity) {
+        cap = capacity;
+    }
+    
+    int get(int key) {
+        if (cache.find(key) == cache.end())
+            return -1;
+        order.erase(cache[key].second);
+        order.push_back(key);
+        cache[key].second = --order.end();
+        return cache[key].first;
+    }
+    
+    void put(int key, int value) {
+        if (cache.find(key) != cache.end()) {
+            order.erase(cache[key].second);
+        } else if (cap == cache.size()) {
+            int lru = order.front();
+            order.pop_front();
+            cache.erase(lru);
+        }
+        order.push_back(key);
+        cache[key] = {value, --order.end()};
+    }
+};
