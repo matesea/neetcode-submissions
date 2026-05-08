3c475f83c662338869414de6287cd73d254d26d2
Wen-Long(Neil) Yang <matesea@gmail.com>
2026-05-08 17:42:50 +0800

    Add: surrounded-regions - submission-2

---
+++ Data Structures & Algorithms/surrounded-regions/submission-2.cpp
@@ -0,0 +1,32 @@
+class Solution {
+    void dfs(vector<vector<char>>& board, int i, int j) {
+        int m = board.size(), n = board.front().size();
+        if (i < 0 || i >= m || j < 0 || j >= n || board[i][j] != 'O')
+            return;
+        board[i][j] = 'T';
+        dfs(board, i + 1, j);
+        dfs(board, i - 1, j);
+        dfs(board, i, j + 1);
+        dfs(board, i, j - 1);
+    }
+public:
+    void solve(vector<vector<char>>& board) {
+        int m = board.size(), n = board.front().size();
+        for (int i = 0; i < m; ++i) {
+            dfs(board, i, 0);
+            dfs(board, i, n - 1);
+        }
+        for (int j = 0; j < n; ++j) {
+            dfs(board, 0, j);
+            dfs(board, m-1, j);
+        }
+        for (int i = 0; i < m; ++i) {
+            for (int j = 0; j < n; ++j) {
+                if (board[i][j] == 'T')
+                    board[i][j] = 'O';
+                else if (board[i][j] == 'O')
+                    board[i][j] = 'X';
+            }
+        }
+    }
+};
