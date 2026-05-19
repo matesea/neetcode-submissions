409b4c4e10218665a424b6feef74b206121ddcf2
Wen-Long(Neil) Yang <matesea@gmail.com>
2026-05-19 21:35:52 +0800

    Add: climbing-stairs - submission-0

---
+++ Data Structures & Algorithms/climbing-stairs/submission-0.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    int climbStairs(int n) {
+       vector<int> dp(1+n, 0); 
+       dp[0] = 1;
+        for (int i = 0; i <= n; ++i) {
+            if (i >= 1)
+                dp[i] += dp[i-1];
+            if (i >= 2)
+                dp[i] += dp[i-2];
+        }
+        return dp[n];
+    }
+};
