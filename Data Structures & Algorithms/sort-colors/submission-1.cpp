16f459723c6fe260e845df6a2accedff719b97f1
Wen-Long(Neil) Yang <matesea@gmail.com>
2026-06-02 23:43:12 +0800

    Add: sort-colors - submission-1

---
+++ Data Structures & Algorithms/sort-colors/submission-1.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    void sortColors(vector<int>& nums) {
+        int i = 0, j = 0, k = nums.size() - 1;
+        while (j <= k) {
+            if (nums[j] < 1)
+                swap(nums[i++], nums[j++]);
+            else if (nums[j] > 1) {
+                swap(nums[k--], nums[j]);
+            } else ++j;
+        }
+    }
+};
