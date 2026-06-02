cf822339d4ea4f5b735bedc76e0efdeefeafce30
Wen-Long(Neil) Yang <matesea@gmail.com>
2026-06-02 22:41:30 +0800

    Add: remove-element - submission-0

---
+++ Data Structures & Algorithms/remove-element/submission-0.cpp
@@ -0,0 +1,11 @@
+class Solution {
+public:
+    int removeElement(vector<int>& nums, int val) {
+        int i = 0;
+        for (int j = 0; j < nums.size(); ++j) {
+            if (val != nums[j])
+                nums[i++] = nums[j];
+        }
+        return i;
+    }
+};
